29ef2b3b388b0f335eaaa763041bf51f0ed641cd
narayanavarma-maker <narayanaraju2024@ssim.ac.in>
2025-08-03 19:36:14 +0530

    Update builder.tsx

--- src/app/[[...page]]/src/components/builder.tsx
+++ src/app/[[...page]]/src/components/builder.tsx
@@ -1,9 +1,13 @@
 'use client';
 import { BuilderComponent, useIsPreviewing } from '@builder.io/react';
+import { builder } from '@builder.io/sdk';
 
 interface BuilderPageProps {
   content: any;
 }
+
+// Replace with your Public API Key
+builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY!);
 
 export function RenderBuilderContent({ content }: BuilderPageProps) {
   const isPreviewing = useIsPreviewing();
